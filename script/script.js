a79760380dc958029782ebee5c80b79b1b616077
Kanchana K. <162160183+Kanchana-Kaushal@users.noreply.github.com>
2025-02-18 19:20:55 +0530

    button animation

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 document.querySelector(".js-share-btn").addEventListener("click", () => {
   document.querySelectorAll(".js-share-popup").forEach((popup) => {
     popup.classList.toggle("opacity-100");
+    popup.classList.toggle("md:-translate-y-7");
   });
 });
